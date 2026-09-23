dbc356a5490b4d4e2310541fcc8922b3ceffaef8
NewAdmin <newadmin@Mariyahs-MacBook.local>
2026-09-23 17:45:50 -0400

    warmup4 completed

---
+++ week-3/warmup4.py
@@ -0,0 +1,9 @@
+num=input("Please enter a number: ")
+if int(num)>=0:
+    print("The number is positive.")
+else:
+    print("The number is negative.")
+if(int(num)%2==0):
+    print("The number is even.")
+else:
+    print("The number is odd.")
