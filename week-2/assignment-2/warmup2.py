405784a2f01cd46f8223afe737d90e3c289cccdc
NewAdmin <newadmin@Mariyahs-MacBook.local>
2026-09-23 17:23:57 -0400

    commiting warmup changes to assignment-2 branch

--- week-2/assignment-2/warmup2.py
+++ week-2/assignment-2/warmup2.py
@@ -27,7 +27,3 @@ print("Today's date is", ans)
 num6 = 6
 print(f"{4+num6}")
 
-#Part 2: Mini-Project
-fahr=input("Enter temperature in Fahrenheit: ")
-cels=(float(fahr)-32)*5/9
-print(f"{float(fahr):.1f} degrees Fahrenheit is {cels:.1f} in Celsius")
