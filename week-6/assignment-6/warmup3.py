b6fe8383ecfc01124546f0b04e5f53ff70a4b35b
NewAdmin <newadmin@Mariyahs-MacBook.local>
2026-09-25 16:14:01 -0400

    w3 complete

--- week-6/assignment-6/warmup3.py
+++ week-6/assignment-6/warmup3.py
@@ -1,6 +1,10 @@
+# A function to show the scope of a variable
 def showScope():
     x = 10  # Local variable
     return x
 # NameError: name 'x' is not defined
 # ans=x+5
-print(f"answer is {showScope()+5}")  # Output: answer is 15
+
+ans=showScope()
+print(f"answer is {ans+5}")  # Output: answer is 15
+
